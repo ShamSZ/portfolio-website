f55867ce38221762d06c13e81396b6b016d14742
ShamSZ <sham.szoda@gmail.com>
2019-01-11 15:48:51 +0000

    Enlarged project-box to show single project per page. Added project data as an array of objects.

---
+++ projects.js
@@ -0,0 +1,38 @@
+const projectsData = [
+  {
+    name: 'Warp Factor',
+    text1: 'Warp Factor is an entertaining browser-based arcade-style game. Challenge your reaction skills by avoiding the obstacles as you travel at Warp Factor speeds through space and accumulate bonus points to make into the Hall of Fame.',
+    text2: 'This was my first project from General Assembly\'s Web Development Immersive Course. It was an individual project built in a week, and was both the first proper game I had built, and my first real-world type practice with JavaScript.',
+    launchLink: 'https://shamsz.github.io/wdi-project-one/',
+    repoLink: 'https://github.com/ShamSZ/wdi-project-one',
+    stack: 'HTML5, CSS3, JavaScript',
+    images: ['images/warp-factor2.png']
+  },
+  {
+    name: 'Rating Ninja',
+    text1: 'Rating Ninja is a web application that uses MongoDB, Express.js and Node.js to store a collection of user added data such as Restaurants. It utilises user authentication to register and log in a user to allow them to make changes to their own data and to add reviews to all restaurants.',
+    text2: 'Rating Ninja was my second project from General Assembly\'s Web Development Immersive. It was an individual project built in one week, and was my first multi-page, RESTful web app that didn\'t only involve working with the front-end. Working on this project helped me understand the basics of working with databases using Node.js packages and the MVC model.',
+    launchLink: 'https://rating-ninja.herokuapp.com/',
+    repoLink: 'https://github.com/ShamSZ/wdi-project-two',
+    stack: 'EJS, MongoDB, Express.js, Node.js',
+    images: ['images/rating-ninja.png']
+  },
+  {
+    name: 'EventZ',
+    text1: 'EventZ - is an online event planner app. Users can browse, create, attend and keep track of events. It is designed mobile-first and displays the location of an event on the map.',
+    text2: 'This was my third project as part of the General Assembly Web Development Immersive course. The objective was to build a full stack(MEAN), RESTful web application using AngularJS in one week.',
+    launchLink: 'https://eventz-planner.herokuapp.com/#!/',
+    repoLink: 'https://github.com/ShamSZ/wdi-project-three',
+    stack: 'HTML5, MongoDB, Express.js, AngularJS, Node.js, OpenStreetMap API, Mocha & Chai',
+    images: ['images/eventz-planner.png']
+  },
+  {
+    name: 'Play & Watch',
+    text1: 'Play & Watch is an e-commerce web app that sells video games and movies. Users can register and keep track of their purchase history, have tailored suggestions made to them in the form of featured products and can leave their ratings. Administrator accounts are able to see all the purchases made and keep track of sales on the statistics page.',
+    text2: 'This was my fourth project as part of the General Assembly Web Development Immersive course. The objective was to build a full stack(MERN), RESTful web application using React.js in one week.',
+    launchLink: 'https://play-and-watch.herokuapp.com/',
+    repoLink: 'https://github.com/ShamSZ/wdi-project-four',
+    stack: 'HTML5, MongoDB, Express.js, React.js, Node.js, Mocha & Chai',
+    images: ['images/play-and-watch.png']
+  }
+];
